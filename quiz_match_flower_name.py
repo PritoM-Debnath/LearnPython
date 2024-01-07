49b4d8c39156b11c1b05cf8e07d93545e84ae05a
Pritom Debnath <debnathpritom@outlook.com>
2024-01-08 01:04:23 +0600

    Modified quiz_match_flower_name.py

--- quiz_match_flower_name.py
+++ quiz_match_flower_name.py
@@ -1,3 +1,6 @@
+'''Create a function that opens the flowers.txt, reads every line in it, and saves it as a dictionary. The main (separate) function should take 
+user input (user's first name and last name) and parse the user input to identify the first letter of the first name. It should then use it to print 
+the flower name with the same first letter (from dictionary created in the first function).'''
 # Write your code here
 def read_flower_name(file_path):
     flower_dict = {}
